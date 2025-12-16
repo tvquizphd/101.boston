87ab675d449e253a1e49cdddccf9695cd110851d
John Hoffer <john@hoff.in>
2025-12-16 14:41:17 +0000

    all UI elements in place

---
+++ rollup.config.mjs
@@ -0,0 +1,45 @@
+import resolve from '@rollup/plugin-node-resolve';
+import { cssModules } from 'rollup-plugin-css-modules';
+import commonjs from '@rollup/plugin-commonjs';
+import terser from '@rollup/plugin-terser';
+import babel from '@rollup/plugin-babel';
+import alias from '@rollup/plugin-alias';
+import path from 'path';
+
+export default {
+  input: 'lib/index.js',
+  plugins: [
+    resolve(),
+    alias({
+      entries: [
+        { find: '@lib', replacement: path.resolve(import.meta.dirname, 'lib') },
+      ],
+    }),
+    babel({
+      exclude: 'node_modules/**',
+      babelHelpers: "bundled",
+      presets: [
+        [
+          '@babel/env',
+          {
+            modules: false,
+            targets: {
+              browsers: '> 1%, IE 11, not op_mini all, not dead',
+              node: 8
+            },
+            useBuiltIns: 'usage',
+            corejs: 3
+          }
+        ]
+      ]
+    }),
+    commonjs(),
+    cssModules(),
+    terser()
+  ],
+  output: {
+    file: 'dist/bundle.js',
+    format: 'es',
+    sourcemap: false
+  } 
+};
